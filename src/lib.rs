e9751c3b31e15e1c4e77e8f3e933e3f3aef3ce2f
Louis Abraham <louis.abraham@yahoo.fr>
2023-07-28 23:20:03 +0200

    intial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,78 @@
 #![no_std]
 
 multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
 
 #[multiversx_sc::contract]
-pub trait Contract {
+pub trait LimitOrderContract {
     #[init]
     fn init(&self) {}
+
+    #[endpoint]
+    #[payable("*")]
+    fn create_order(&self, price: BigUint) -> usize {
+        let caller: ManagedAddress = self.blockchain().get_caller();
+        let payment: EgldOrEsdtTokenPayment = self.call_value().egld_or_single_esdt();
+        self.orders().push(&Order {
+            owner: caller,
+            payment,
+            price,
+            active: true,
+        });
+        self.orders().len()
+    }
+
+    #[endpoint]
+    fn cancel_order(&self, order_id: usize) {
+        let caller: ManagedAddress = self.blockchain().get_caller();
+        let mut order: Order<Self::Api> = self.orders().get(order_id);
+        assert!(order.owner == caller, "Only the owner can cancel the order");
+        assert!(order.active, "The order is already cancelled");
+        order.active = false;
+        self.orders().set(order_id, &order);
+        self.send().direct(
+            &caller,
+            &order.payment.token_identifier,
+            order.payment.token_nonce,
+            &order.payment.amount,
+        );
+    }
+
+    #[endpoint]
+    #[payable("EGLD")]
+    fn fill_order(&self, order_id: usize) {
+        let mut order: Order<Self::Api> = self.orders().get(order_id);
+        assert!(order.active, "The order has been cancelled");
+        let paid_amount = self.call_value().egld_value().clone_value();
+        assert!(
+            paid_amount >= order.price,
+            "Not enough amount to fill the order"
+        );
+        let caller = self.blockchain().get_caller();
+        self.send().direct(
+            &caller,
+            &order.payment.token_identifier,
+            order.payment.token_nonce,
+            &order.payment.amount,
+        );
+        self.send().direct(
+            &order.owner,
+            &EgldOrEsdtTokenIdentifier::egld(),
+            0,
+            &paid_amount,
+        );
+        order.active = false;
+        self.orders().set(order_id, &order);
+    }
+
+    #[storage_mapper("orders")]
+    fn orders(&self) -> VecMapper<Order<Self::Api>>;
+}
+
+#[derive(TopEncode, TopDecode, Debug)]
+pub struct Order<M: ManagedTypeApi> {
+    owner: ManagedAddress<M>,
+    payment: EgldOrEsdtTokenPayment<M>,
+    price: BigUint<M>,
+    active: bool,
 }
--- tests/contract.test.ts
+++ tests/contract.test.ts
@@ -1,28 +1,77 @@
 import { test, beforeEach, afterEach } from "vitest";
 import { assertAccount } from "xsuite/assert";
+import { e } from "xsuite/data";
 import { FWorld, FWorldWallet, FWorldContract } from "xsuite/world";
 
 let world: FWorld;
 let deployer: FWorldWallet;
 let contract: FWorldContract;
+let sender: FWorldWallet;
+let receiver: FWorldWallet;
+const egldId = "EGLD";
+const sftId = "SFT-abcdef";
 
 beforeEach(async () => {
-  world = await FWorld.start();
-  deployer = await world.createWallet();
-  ({ contract } = await deployer.deployContract({
-    code: "file:output/contract.wasm",
-    codeMetadata: [],
-    gasLimit: 10_000_000,
-  }));
+    world = await FWorld.start();
+    deployer = await world.createWallet();
+    ({ contract } = await deployer.deployContract({
+        code: "file:output/contract.wasm",
+        codeMetadata: [],
+        gasLimit: 10_000_000,
+    }));
+    sender = await world.createWallet({
+        pairs: [e.p.Esdts([{ id: sftId, nonce: 1, amount: 100_000 }])],
+    });
+    receiver = await world.createWallet({ balance: 100_000 });
 });
 
 afterEach(async () => {
-  await world.terminate();
+    await world.terminate();
 });
 
 test("Test", async () => {
-  assertAccount(await contract.getAccountWithPairs(), {
-    balance: 0n,
-    hasPairs: [],
-  });
+    assertAccount(await contract.getAccountWithPairs(), {
+        balance: 0n,
+        hasPairs: [],
+    });
+    const contractId = (
+        await sender.callContract({
+            callee: contract,
+            funcName: "create_order",
+            funcArgs: [e.U(100_000)],
+            esdts: [{ id: sftId, nonce: 1, amount: 100_000 }],
+            gasLimit: 10_000_000,
+        })
+    ).returnData[0];
+    console.log("contractId", contractId);
+
+    assertAccount(await contract.getAccountWithPairs(), {
+        balance: 0n,
+        hasPairs: [e.p.Esdts([{ id: sftId, nonce: 1, amount: 100_000 }])],
+    });
+    assertAccount(await sender.getAccountWithPairs(), {
+        balance: 0n,
+        hasPairs: [],
+    });
+    console.log("contract created successfully");
+    await receiver.callContract({
+        callee: contract,
+        funcName: "fill_order",
+        funcArgs: [contractId],
+        value: 100_000,
+        gasLimit: 10_000_000,
+    });
+    console.log("contract filled successfully");
+    assertAccount(await sender.getAccountWithPairs(), {
+        balance: 100_000n,
+        hasPairs: [],
+    });
+    assertAccount(await contract.getAccountWithPairs(), {
+        balance: 0n,
+        hasPairs: [e.p.Esdts([{ id: sftId, nonce: 1, amount: 0 }])],
+    });
+    assertAccount(await receiver.getAccountWithPairs(), {
+        balance: 0n,
+        hasPairs: [e.p.Esdts([{ id: sftId, nonce: 1, amount: 100_000 }])],
+    });
 });
